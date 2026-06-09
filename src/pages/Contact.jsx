c76c2dd0bd15b60bc0dbe9759f95214b806aadc1
esbergenova <muxtartadjgaliev6@gmail.com>
2026-06-09 14:19:06 +0500

    add phone

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -89,7 +89,8 @@ export default function Contact() {
               <Phone className="w-5 h-5 text-primary flex-shrink-0 mt-0.5" />
               <div>
                 <h3 className="font-semibold mb-1">{t('contact.phone')}</h3>
-                <p className="text-sm text-muted-foreground">+998 93 614 47 44</p>
+<p className="text-sm text-muted-foreground">+998 93 614 47 44</p>
+<p className="text-sm text-muted-foreground">+998 90 594 95 37</p>
               </div>
             </div>
           </motion.div>
